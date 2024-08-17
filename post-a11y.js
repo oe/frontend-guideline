8e80b02e5d402b6a98e75398b5137bdaaa0950f1
oe <okarl@outlook.com>
2024-08-17 10:24:50 +0000

    oe published a site update

--- post-a11y.js
+++ post-a11y.js
@@ -1,8 +1,6 @@
 document.body.querySelectorAll('.sl-container img').forEach((img) => {
   const title = img.getAttribute('alt');
   if (!title) return;
-  // remove for seo, as it's already in the figcaption
-  img.removeAttribute('alt');
   const figure = document.createElement('figure');
   img.parentNode.replaceChild(figure, img);
   figure.appendChild(img);
